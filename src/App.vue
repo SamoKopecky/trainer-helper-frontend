e3a0b617775b52bae98050a276f469059e4716d6
SamoKopecky <sano.kopecky@gmail.com>
2025-02-01 19:08:16 +0100

    feat: Add playground stuff
    Signed-off-by: SamoKopecky <sano.kopecky@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,39 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import ButtonComponent from './components/ButtonComponent.vue'
+
+const test = 1
+const disabled = ref(false)
+const rawHtml = '<span style="color: red">This is red</span>'
+
+function setDisabled() {
+  console.log('clicked')
+  disabled.value = !disabled.value
+}
+
+const obj = ref({
+  nested: { count: 0 },
+  arr: ['foo', 'bar'],
+})
 </script>
 
 <template>
-  <h1>Trainer app</h1>
-</template>
+  <h1>Trainer app {{ test }}</h1>
+  <p>Using text interpolation: {{ rawHtml }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+  <button :disabled>This is a disabled button</button>
+  <button @click="setDisabled">Disable the button</button>
 
-<style scoped></style>
+  <br />
+  <br />
+  <button></button>
+  <button @click="obj.nested.count++">Update obj</button>
+  <div>
+    <p>{{ obj.arr }}</p>
+    <p>{{ obj.nested }}</p>
+  </div>
+
+  <div>
+    <ButtonComponent />
+  </div>
+</template>
