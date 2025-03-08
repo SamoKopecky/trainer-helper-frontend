55a56776abd55cd98fc13a2f2a46ee2a0d4595d8
SamoKopecky <sano.kopecky@gmail.com>
2025-03-08 18:26:27 +0100

    feat: Add test
    Signed-off-by: SamoKopecky <sano.kopecky@gmail.com>

--- src/utils/date.test.ts
+++ src/utils/date.test.ts
@@ -1,7 +1,12 @@
 import { test, expect } from "vitest"
-import { isoToLocal } from "./date"
+import { getISODateString, isoToLocal } from "./date"
 
 test("isoToLocal", () => {
   const isoString = "2025-03-08T12:00:00"
   expect(isoToLocal(isoString)).toEqual(new Date("2025-03-08T13:00:00"))
+})
+
+test("getISODateString", () => {
+  const date = new Date("2025-03-08T12:00:00")
+  expect(getISODateString(date)).toEqual("2025-03-08T11:00:00")
 })
